5a627a8ce6cdde325c9805d42f83ad8545b91951
Luca Anthony <lucamafort05@gmail.com>
2024-05-01 14:46:07 -0300

    Incremento de animação em panqueca

--- game/scripts/goal.gd
+++ game/scripts/goal.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 @onready var transition = $"../transition"
 @export var next_level : String = ''
+@onready var sprite = $sprite
 
 func _on_body_entered(body):
 	if body.name == 'player' and next_level != '':
+		sprite.play("butter_loop")
+
+
+func _on_sprite_animation_finished():
+	if sprite.animation == 'butter_loop':
 		transition.change_scene(next_level)
